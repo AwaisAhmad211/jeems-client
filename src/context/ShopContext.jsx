21b7c8c54a6fbdad9ed3cee7d818eea6e30cddb8
awaisahmad211 <awaisahmad.codes@gmail.com>
2026-07-18 23:28:01 +0500

    Changed from "react" to "axios" to fix crashes

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import axios from "axios"; // FIXED: Changed from "react" to "axios" to fix crashes
+import axios from "axios"; 
 import axiosInstance from "../utils/axiosInstance";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
