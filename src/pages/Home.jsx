039225ca7c34476ac8f03a728300566610dcece6
awaisahmad211 <awaisahmad.codes@gmail.com>
2026-07-22 21:23:24 +0500

    Hero carousel added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,12 @@ import BestSeller from "../components/BestSeller";
 import OurPolicy from "../components/OurPolicy";
 import NewsletterBox from "../components/NewsletterBox";
 import ShopByCategory from "../components/ShopByCategory";
-import HeroCarousel from "../components/HeroCarousel";
+// import HeroCarousel from "../components/HeroCarousel";
 
 const Home = () => {
   return (
     <div className="page-transition">
-      <HeroCarousel />
+      {/* <HeroCarousel /> */}
       <Hero />
       <ShopByCategory />
       <LatestCollection />
